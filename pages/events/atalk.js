064b574c81cd2dc95edc5c8ab3d8dd7abc16c532
lutfiandri <lutfiandri232@gmail.com>
2023-06-22 10:27:35 +0700

    event atalk comvis grand

--- pages/events/atalk.js
+++ pages/events/atalk.js
@@ -1,6 +1,6 @@
 import Navbar from '../../components/navbar';
 
-export default function CompanyVisit() {
+export default function Atalk() {
   return (
     <div>
       <Navbar fixedBg></Navbar>
@@ -10,8 +10,9 @@ export default function CompanyVisit() {
       {/* content */}
       <div className='w-full min-h-screen-no-header bg-c-blue text-white'>
         <div className='mx-auto container bg-c-blue min-h-screen-no-header grid place-items-center'>
-          <div className='bg-event-card-comvis bg-cover w-full aspect-[16/10] flex flex-col justify-center items-center p-32 drop-shadow-lg'>
-            <h2 className='font-belleza text-6xl text-center'>Company Visit</h2>
+          <div className='bg-event-card-atalk bg-cover w-full aspect-[16/10] flex flex-col justify-center items-center p-32 drop-shadow-lg'>
+            <h3 className='font-belleza text-3xl text-center text-c-orange'>A-Talk</h3>
+            <h2 className='font-belleza text-6xl text-center'>Accounting Talk</h2>
             <div className='h-6'></div>
             <p className='text-center text-lg'>As a part of the Jogjakarta National Case Competition (JNCC), Company Visit aims to give insights and networking experience for the semi-finalists to expand their knowledge on ethical standards and sustainable practices in supply chains with speakers from a reputable company who will also share about their company and working experience.</p>
           </div>
---
+++ pages/events/grand.js
@@ -0,0 +1,23 @@
+import Navbar from '../../components/navbar';
+
+export default function CompanyVisit() {
+  return (
+    <div>
+      <Navbar fixedBg></Navbar>
+      {/* for navbar placeholder */}
+      <div className='h-[82px]'></div>
+
+      {/* content */}
+      <div className='w-full min-h-screen-no-header bg-c-blue text-white'>
+        <div className='mx-auto container bg-c-blue min-h-screen-no-header grid place-items-center'>
+          <div className='bg-event-card-comvis bg-cover w-full aspect-[16/10] flex flex-col justify-center items-center p-32 drop-shadow-lg'>
+            <h3 className='font-belleza text-3xl text-center text-c-orange'>GRAND</h3>
+            <h2 className='font-belleza text-6xl text-center'>GMAD Awarding and Networking Day</h2>
+            <div className='h-6'></div>
+            <p className='text-center text-lg'>GRAND (GMAD Awarding and Networking Day) is a new breakthrough in the series of events of GMAD 2023, which serves as the culmination, awarding, and closing ceremony of the entire series of competitions of GMAD. This event is accompanied by various exciting activities such as performances, networking sessions, and games that will enhance the sense of togetherness and serve as entertainment for all participants and committees of GMAD 2023.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
