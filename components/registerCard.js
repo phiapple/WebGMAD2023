2cde97c1f1e6efacd016b61d77d78ad5ce984bf9
lutfiandri <lutfiandri232@gmail.com>
2023-06-23 10:27:49 +0700

    register swiper (laptop only)

--- components/registerCard.js
+++ components/registerCard.js
@@ -1,19 +1,33 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 
-export default function RegisterCard({ date, title, registerUrl, detailUrl }) {
+export default function RegisterCard({
+  date,
+  title,
+  registerUrl,
+  detailUrl,
+  bg,
+}) {
   return (
-    <div className="flex aspect-[48/56] w-[400px] flex-col-reverse rounded-lg  bg-c-white bg-event-card-atalk bg-cover bg-center p-6 text-c-white drop-shadow-lg">
+    <div
+      className={clsx(
+        'bg-c-whitebg-cover m-0 flex aspect-[7/8] h-[40vh] flex-col-reverse rounded-2xl bg-center p-6  text-c-white drop-shadow-lg md:h-[60vh] lg:h-[50vh]',
+        bg
+      )}
+    >
       <div>
-        <div className="mb-3">{date}</div>
-        <h2 className="mb-5 text-3xl font-semibold">{title}</h2>
+        <div className="mb-2">{date}</div>
+        <h2 className="mb-4 text-2xl font-semibold">{title}</h2>
         <div className="flex gap-4">
-          <Link
-            href={registerUrl}
-            target="_blank"
-            className="w-full rounded-full bg-c-orange p-2 text-center font-semibold text-c-white"
-          >
-            Register Now
-          </Link>
+          {registerUrl && (
+            <Link
+              href={registerUrl}
+              target="_blank"
+              className="w-full rounded-full bg-c-orange p-2 text-center font-semibold text-c-white"
+            >
+              Register Now
+            </Link>
+          )}
           <Link
             href={detailUrl}
             className="w-full rounded-full border-2 border-c-white p-2 text-center font-semibold text-c-white"
--- pages/register.js
+++ pages/register.js
@@ -4,6 +4,11 @@ import RegisterCard from '../components/registerCard';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
 import { useState } from 'react';
+import { BiChevronRightCircle, BiChevronLeftCircle } from 'react-icons/bi';
+
+import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 
 // JNCC	:  https://bit.ly/RegistrationJNCC2023
 // A-Talk:  https://bit.ly/RegistrationA-Talk2023
@@ -13,27 +18,50 @@ import { useState } from 'react';
 const EVENTS = [
   {
     title: 'National Accounting Olympiad',
-    date: '22-23 September 2023',
-    registerUrl: 'https://bit.ly/RegistrationJNCC2023',
-    detailUrl: '/events/jncc',
-  },
-  {
-    title: 'Atalk',
-    date: '22-23 September 2023',
-    registerUrl: 'https://bit.ly/RegistrationA-Talk2023',
-    detailUrl: '/events/atalk',
+    date: 'Until 17 August 2023',
+    registerUrl: 'https://bit.ly/RegistrationNAO2023',
+    detailUrl: '/events/nao',
+    bg: `bg-[url('/images/card/nao-grad.png')]`,
   },
   {
     title: 'Junior National Accounting Olympiad',
     date: '22-23 September 2023',
     registerUrl: 'https://bit.ly/RegistrationNAOJunior2023',
     detailUrl: '/events/junior-nao',
+    bg: `bg-[url('/images/card/jnao-grad.png')]`,
   },
   {
-    title: 'National Accounting Olympiad',
+    title: 'Jogjakarta National Case Competition',
     date: '22-23 September 2023',
-    registerUrl: 'https://bit.ly/RegistrationNAO2023',
-    detailUrl: '/events/nao',
+    registerUrl: 'https://bit.ly/RegistrationJNCC2023',
+    detailUrl: '/events/jncc',
+    bg: `bg-[url('/images/card/jncc-grad.png')]`,
+  },
+  {
+    title: 'Atalk',
+    date: '22 September 2023',
+    registerUrl: 'https://bit.ly/RegistrationA-Talk2023',
+    detailUrl: '/events/atalk',
+    bg: `bg-[url('/images/card/atalk-grad.png')]`,
+  },
+
+  {
+    title: 'GMAD Awarding and Networking Day',
+    date: '22-23 September 2023',
+    detailUrl: '/events/grand',
+    bg: `bg-[url('/images/card/grand-grad.png')]`,
+  },
+  {
+    title: 'Company Visit',
+    date: '22-23 September 2023',
+    detailUrl: '/events/comvis',
+    bg: `bg-[url('/images/card/comvis-grad.png')]`,
+  },
+  {
+    title: 'Training',
+    date: '22-23 September 2023',
+    detailUrl: '/events/training',
+    bg: `bg-[url('/images/card/training-grad.png')]`,
   },
 ];
 
@@ -49,37 +77,57 @@ export default function Register() {
       <div className="h-[82px]"></div>
 
       {/* content */}
-      <div className="min-h-screen-no-header w-full bg-c-blue text-white">
+      <div className="min-h-screen-no-header relative w-full bg-gradient-to-b from-c-blue to-c-green text-white">
         <div className="container">
+          <div className="min-h-screen-no-header flex w-[300px] flex-col items-center justify-center gap-6">
+            <h1 className="text-center font-belleza text-6xl">
+              Main <br />
+              Event
+            </h1>
+            <div className="flex gap-4">
+              <BiChevronLeftCircle
+                onClick={() => swiper.slidePrev()}
+                className="h-10 w-10 cursor-pointer"
+              ></BiChevronLeftCircle>
+              <BiChevronRightCircle
+                onClick={() => swiper.slideNext()}
+                className="h-10 w-10 cursor-pointer"
+              ></BiChevronRightCircle>
+            </div>
+          </div>
+        </div>
+
+        <div className="absolute right-0 top-[20%]">
           <Swiper
-            centeredSlides
-            slidesPerView="auto"
-            spaceBetween={10}
+            id="swiper-register"
+            className="absolute w-[800px]"
+            slidesPerView={'auto'}
             pagination={{
               clickable: true,
             }}
-            modules={[Pagination]}
-            className="flex w-full flex-row"
+            // modules={[Pagination]}
             onSwiper={(swiper) => setSwiper(swiper)}
+            grabCursor
             breakpoints={{
-              100: {
+              0: {
                 slidesPerView: 2,
-                spaceBetween: 20,
+                spaceBetween: 10,
               },
               1024: {
-                slidesPerView: 3,
+                slidesPerView: 2,
                 spaceBetween: 10,
               },
             }}
           >
             {EVENTS.map((event, index) => {
               return (
-                <SwiperSlide className="h-full" key={index}>
+                <SwiperSlide key={index}>
                   <RegisterCard
                     date={event.date}
                     detailUrl={event.detailUrl}
                     registerUrl={event.registerUrl}
                     title={event.title}
+                    bg={event.bg}
                   ></RegisterCard>
                 </SwiperSlide>
               );
