5c90286153d71948cac34737f814daff41e7acee
lutfiandri <lutfiandri232@gmail.com>
2023-06-24 21:38:42 +0700

    contact responsive

--- pages/contact.js
+++ pages/contact.js
@@ -46,58 +46,61 @@ export default function Contact() {
         <div className="flex flex-col bg-c-blue text-c-white">
           <div className="min-h-screen-no-header-footer bg-c-blue">
             <div className="relative h-[40vh] bg-c-blue bg-opacity-10 bg-[url('/images/contact-map.png')]">
-              {/* <div className="container relative z-[99] bg-white"> */}
               <div className="container relative h-full">
                 <h1 className="absolute bottom-8 font-belleza text-6xl drop-shadow-md">
                   Contact Us
                 </h1>
-
-                {/* floating right */}
-                <div className="absolute bottom-0 right-[15%] translate-y-[50%] rounded-xl bg-c-white px-8 py-12 text-c-blue drop-shadow-lg">
-                  <h2 className="text-xl font-bold">Contact Us At</h2>
-                  <div className="mt-6">
-                    <h3 className="mb-2 border-b-2 border-b-c-blue font-semibold">
-                      Sponsorship
-                    </h3>
-                    <Link
-                      href="https://wa.me/6281511115195"
-                      target="_blank"
-                      className="flex w-64 items-center justify-center gap-2 rounded-full bg-c-green p-2 text-2xl font-semibold text-c-white drop-shadow-md"
-                    >
-                      <BsWhatsapp className="h-6 w-6"></BsWhatsapp>
-                      <div>081511115195</div>
-                    </Link>
-                  </div>
-                  <div className="mt-6">
-                    <h3 className="mb-2 border-b-2 border-b-c-blue font-semibold">
-                      Media Partnership
-                    </h3>
-                    <Link
-                      href="https://wa.me/6285800318595"
-                      target="_blank"
-                      className="flex w-64 items-center justify-center gap-2 rounded-full bg-c-green p-2 text-2xl font-semibold text-c-white drop-shadow-md"
-                    >
-                      <BsWhatsapp className="h-6 w-6"></BsWhatsapp>
-                      <div>085800318595</div>
-                    </Link>
-                  </div>
-                </div>
               </div>
             </div>
-            <div className="container mt-16 flex-1 font-medium">
-              <h2 className="text-xl font-semibold">Also Find Us At</h2>
-              <div className="h-4"></div>
-              <div className="flex flex-col gap-4">
-                {CONTACTS.map((contact) => (
+
+            <div className="container relative mt-16 flex-1 font-medium">
+              {/* floating right */}
+              <div className=" top-0 rounded-xl bg-c-white px-8 py-12 text-c-blue drop-shadow-lg lg:absolute lg:right-[15%] lg:translate-y-[-50%]">
+                <h2 className="text-xl font-bold">Contact Us At</h2>
+                <div className="mt-6">
+                  <h3 className="mb-2 border-b-2 border-b-c-blue font-semibold">
+                    Sponsorship
+                  </h3>
                   <Link
-                    href={contact.url}
+                    href="https://wa.me/6281511115195"
                     target="_blank"
-                    className="flex items-center gap-3"
+                    className="flex w-64 items-center justify-center gap-2 rounded-full bg-c-green p-2 text-2xl font-semibold text-c-white drop-shadow-md"
                   >
-                    {contact.icon}
-                    <div className="font-medium">{contact.name}</div>
+                    <BsWhatsapp className="h-6 w-6"></BsWhatsapp>
+                    <div>081511115195</div>
                   </Link>
-                ))}
+                </div>
+                <div className="mt-6">
+                  <h3 className="mb-2 border-b-2 border-b-c-blue font-semibold">
+                    Media Partnership
+                  </h3>
+                  <Link
+                    href="https://wa.me/6285800318595"
+                    target="_blank"
+                    className="flex w-64 items-center justify-center gap-2 rounded-full bg-c-green p-2 text-2xl font-semibold text-c-white drop-shadow-md"
+                  >
+                    <BsWhatsapp className="h-6 w-6"></BsWhatsapp>
+                    <div>085800318595</div>
+                  </Link>
+                </div>
+              </div>
+
+              <div className="hidden lg:block">
+                <h2 className="text-xl font-semibold">Also Find Us At</h2>
+                <div className="h-4"></div>
+                <div className="flex flex-col gap-4">
+                  {CONTACTS.map((contact) => (
+                    <Link
+                      key={contact.url}
+                      href={contact.url}
+                      target="_blank"
+                      className="flex items-center gap-3"
+                    >
+                      {contact.icon}
+                      <div className="font-medium">{contact.name}</div>
+                    </Link>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
