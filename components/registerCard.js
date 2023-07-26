bfdc8dfc686243f3c7772c4f40d18d2051bd9a45
jovian <alphonsusjovian@gmail.com>
2023-07-26 09:22:38 +0700

    fix: register card

--- components/registerCard.js
+++ components/registerCard.js
@@ -1,41 +1,37 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 
-export default function RegisterCard({
-  date,
-  title,
-  registerUrl,
-  detailUrl,
-  bg,
-}) {
-  return (
-    <div
-      className={clsx(
-        'bg-c-whitebg-cover m-0 flex aspect-[7/8] w-[70vw] max-w-[300px] flex-col-reverse rounded-2xl bg-center p-6 text-c-white  drop-shadow-lg sm:w-[400px] lg:w-[500px]',
-        bg
-      )}
-    >
-      <div>
-        <div className="md:text-md mb-2 text-sm">{date}</div>
-        <h2 className="mb-4 text-xl font-semibold md:text-2xl">{title}</h2>
-        <div className="flex gap-4">
-          {registerUrl && (
-            <Link
-              href={registerUrl}
-              target="_blank"
-              className="md:text-md grid w-full place-items-center rounded-full bg-c-orange p-1.5 text-center text-sm font-semibold text-c-white md:p-2"
-            >
-              Register Now
-            </Link>
-          )}
-          <Link
-            href={detailUrl}
-            className="md:text-md grid w-full place-items-center rounded-full border-2 border-c-white p-1.5 text-center text-sm font-semibold text-c-white md:p-2"
-          >
-            See Detail
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+export default function RegisterCard({ date, title, registerUrl, detailUrl, bg }) {
+	return (
+		<div className="flex flex-col gap-4">
+			<div
+				className={clsx(
+					'bg-c-whitebg-cover m-0 flex aspect-[7/8] w-[70vw] max-w-[300px] flex-col-reverse rounded-2xl bg-center p-6 text-c-white  drop-shadow-lg sm:w-[400px] lg:w-[500px]',
+					bg
+				)}
+			>
+				<div>
+					<div className="md:text-md mb-2 text-sm">{date}</div>
+					<h2 className="mb-4 text-xl font-semibold md:text-2xl">{title}</h2>
+				</div>
+			</div>
+			<div className="flex w-[70vw] max-w-[300px] gap-4 ">
+				{registerUrl && (
+					<Link
+						href={registerUrl}
+						target="_blank"
+						className="md:text-md grid w-full place-items-center rounded-full bg-c-orange p-1.5 text-center text-sm font-semibold text-c-white md:p-2"
+					>
+						Register Now
+					</Link>
+				)}
+				<Link
+					href={detailUrl}
+					className="md:text-md grid w-full place-items-center rounded-full border-2 border-c-white p-1.5 text-center text-sm font-semibold text-c-white md:p-2"
+				>
+					See Detail
+				</Link>
+			</div>
+		</div>
+	);
 }
