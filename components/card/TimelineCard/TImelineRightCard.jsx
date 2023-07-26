fef0c40f369b8f38437e80f234d5074a676a73d9
jovian <alphonsusjovian@gmail.com>
2023-07-26 15:46:28 +0700

    fix(timeline): time align

--- components/card/TimelineCard/TImelineRightCard.jsx
+++ components/card/TimelineCard/TImelineRightCard.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const TImelineCard = ({time, title, desc}) => {
-  return (
-    <>
-        <div className="flex-col max-w-[158px] md:max-w-[666px]" data-aos="fade-left">
-            <div className={"flex justify-center rounded-full bg-c-white border border-c-orange md:px-4 px-2 md:py-2 py-0 text-center items-center"}>
-                <h3 className="md:text-2xl sm:text-xl text-lg text-c-orange">
-                    {time}
-                </h3>
-            </div>
-            <div className="flex justify-start mt-4 mb-2">
-                <h3 className="md:text-2xl text-xl text-c-blue font-bold ">
-                    {title}
-                </h3>
-            </div>
-            <div className="flex justify-start mb-4">
-                <h3 className="text-xl text-c-green">
-                    {desc}
-                </h3>
-            </div>
-        </div>
-    </>
-  )
-}
+const TImelineCard = ({ time, title, desc }) => {
+	return (
+		<>
+			<div className="max-w-[158px] flex-col md:max-w-[666px]" data-aos="fade-left">
+				<div
+					className={
+						'flex items-center justify-start rounded-full border border-c-orange bg-c-white px-4 py-0 text-start md:px-4 md:py-2'
+					}
+				>
+					<h3 className="text-lg text-c-orange sm:text-xl md:text-2xl">{time}</h3>
+				</div>
+				<div className="mb-2 mt-4 flex justify-start">
+					<h3 className="text-xl font-bold text-c-blue md:text-2xl ">{title}</h3>
+				</div>
+				<div className="mb-4 flex justify-start">
+					<h3 className="text-xl text-c-green">{desc}</h3>
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default TImelineCard
+export default TImelineCard;
--- components/card/TimelineCard/TimelineLeftCard.jsx
+++ components/card/TimelineCard/TimelineLeftCard.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-const TImelineCard = ({time, title, desc}) => {
-  return (
-    <>
-        <div className="flex-col max-w-[158px] md:max-w-[666px] items-end text-end" data-aos="fade-right">
-            <div className={"flex justify-center rounded-full bg-c-white border border-c-orange md:px-4 px-2 md:py-2 py-0 text-center items-center"}>
-                <h3 className="md:text-2xl sm:text-xl text-lg text-c-orange">
-                    {time}
-                </h3>
-            </div>
-            <div className="flex justify-end mt-4 mb-2 items-end">
-                <h3 className="md:text-2xl text-xl text-c-blue font-bold ">
-                    {title}
-                </h3>
-            </div>
-            {/* <div className="flex justify-end mb-4">
+const TImelineCard = ({ time, title, desc }) => {
+	return (
+		<>
+			<div
+				className="max-w-[158px] flex-col items-end text-end md:max-w-[666px]"
+				data-aos="fade-right"
+			>
+				<div
+					className={
+						'flex items-center justify-center rounded-full border border-c-orange bg-c-white px-4 py-0 text-end md:px-4 md:py-2'
+					}
+				>
+					<h3 className="text-lg text-c-orange sm:text-xl md:text-2xl">{time}</h3>
+				</div>
+				<div className="mb-2 mt-4 flex items-end justify-end">
+					<h3 className="text-xl font-bold text-c-blue md:text-2xl ">{title}</h3>
+				</div>
+				{/* <div className="flex justify-end mb-4">
                 <h3 className="text-xl text-c-green">
                     {desc}
                 </h3>
             </div> */}
-        </div>
-    </>
-  )
-}
+			</div>
+		</>
+	);
+};
 
-export default TImelineCard
+export default TImelineCard;
